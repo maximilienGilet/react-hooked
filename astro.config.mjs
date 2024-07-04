e30520a8129a2bcfd5b4e0e35e6278b3be840fa1
Maximilien Gilet <maximilien248@gmail.com>
2024-07-04 21:10:56 +0200

    feat: add download dropdown button

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://react-hooked.vercel.app",
   integrations: [tailwind(), react()],
 });
-
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,17 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "selector",
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in-down": {
+          "0%": { opacity: 0, transform: "translateY(-10px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.2s ease-out",
+      },
+    },
   },
   plugins: [
     require("@catppuccin/tailwindcss")({
