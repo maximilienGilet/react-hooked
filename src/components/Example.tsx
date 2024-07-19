603cfbec439fc4e06bba0c80251533299612437d
Maximilien Gilet <maximilien248@gmail.com>
2024-07-19 14:34:15 +0200

    feat: add dynamic example section

---
+++ src/components/Example.tsx
@@ -0,0 +1,38 @@
+import clsx from "clsx";
+import { capitalize } from "../utils/strings";
+import { Suspense, lazy } from "react";
+
+function Fallback() {
+  return (
+    <div
+      className="animate-spin inline-block size-6 border-[3px] border-current border-t-transparent text-ctp-pink rounded-full dark:text-ctp-pink"
+      role="status"
+      aria-label="loading"
+    >
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+}
+
+export interface ExampleProps extends React.HTMLAttributes<HTMLDivElement> {
+  name: string;
+}
+
+export default function Example({ name, className }: ExampleProps) {
+  const ExampleComponent = lazy(
+    () => import(`../examples/${capitalize(name)}Example`),
+  );
+  return (
+    <div className="ctp-frappe">
+      <div className={clsx("gap-4 p-4 rounded-xl bg-ctp-base", className)}>
+        <div className="flex flex-auto flex-col justify-center items-center p-4 md:p-5">
+          <div className="flex justify-center">
+            <Suspense fallback={<Fallback />}>
+              <ExampleComponent />
+            </Suspense>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/examples/UseBooleanExample.tsx
@@ -0,0 +1,42 @@
+import clsx from "clsx";
+import useBoolean from "../hooks/useBoolean";
+
+export default function UseBooleanExample() {
+  const { value, setTrue, setFalse, toggle } = useBoolean(false);
+
+  return (
+    <div>
+      <div>
+        The value is &nbsp;
+        <span
+          className={clsx(
+            "p-2 rounded text-ctp-base",
+            value ? "bg-ctp-green" : "bg-ctp-red",
+          )}
+        >
+          {value ? "true" : "false"}
+        </span>
+      </div>
+      <div className="flex gap-2 mt-6">
+        <button
+          onClick={setTrue}
+          className="bg-ctp-green text-ctp-base px-4 py-2 rounded"
+        >
+          Set to true
+        </button>
+        <button
+          onClick={setFalse}
+          className="bg-ctp-red text-ctp-base px-4 py-2 rounded"
+        >
+          Set to false
+        </button>
+        <button
+          onClick={toggle}
+          className="bg-ctp-text text-ctp-base px-4 py-2 rounded"
+        >
+          Toggle
+        </button>
+      </div>
+    </div>
+  );
+}
