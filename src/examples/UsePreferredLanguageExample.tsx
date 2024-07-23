970ed41340bb08156212c8c791bc421477f66873
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-07-23 22:05:10 +0100

    feat: tell user to change their language to observe the effect

--- src/examples/UsePreferredLanguageExample.tsx
+++ src/examples/UsePreferredLanguageExample.tsx
@@ -6,6 +6,12 @@ export default function App() {
   return (
     <section>
       <div className="text-ctp-text">
+        <p>
+          Updates when you{" "}
+          <a href="chrome://settings/languages">
+            change your preferred language, too.
+          </a>
+        </p>
         <p>Language: {language}</p>
       </div>
     </section>
