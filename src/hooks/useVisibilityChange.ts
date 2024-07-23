f84ed44f57f1cbf7be20ecfd94f68a5fd2ceb6a2
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-07-23 22:18:10 +0100

    feat: add useVisibilityChange hook

---
+++ src/hooks/useVisibilityChange.ts
@@ -0,0 +1,31 @@
+import { useSyncExternalStore } from "react";
+
+const useVisibilityChangeSubscribe = (callback: (event: Event) => void) => {
+  document.addEventListener("visibilitychange", callback);
+
+  return () => {
+    document.removeEventListener("visibilitychange", callback);
+  };
+};
+
+const getVisibilityChangeSnapshot = () => {
+  return document.visibilityState;
+};
+
+const getVisibilityChangeServerSnapshot = () => {
+  throw Error("useVisibilityChange is a client-only hook");
+};
+
+/**
+ * A hook that tracks whether the document is visible or not
+ * @returns {boolean} true if document is visible, false otherwise
+ */
+export function useVisibilityChange(): boolean {
+  const visibilityState = useSyncExternalStore(
+    useVisibilityChangeSubscribe,
+    getVisibilityChangeSnapshot,
+    getVisibilityChangeServerSnapshot,
+  );
+
+  return visibilityState === "visible";
+}
