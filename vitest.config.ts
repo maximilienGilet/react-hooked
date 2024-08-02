7c5914080304968357aad6165927a514e81770f3
Maximilien Gilet <maximilien248@gmail.com>
2024-08-02 10:19:18 +0200

    feat: init tests with useBoolean
    Thanks @DGouron !

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+/// <reference types="vitest" />
+import { getViteConfig } from "astro/config";
+
+export default getViteConfig({
+  test: {
+    // Vitest configuration options
+    environment: "jsdom",
+    globals: true,
+  },
+});
