d9f19c28e40b32e634fea86708976615765b6921
Maximilien Gilet <maximilien248@gmail.com>
2024-08-02 14:55:42 +0200

    feat: add sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://react-hooked.vercel.app",
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()]
 });
